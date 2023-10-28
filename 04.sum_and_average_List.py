39c2d8600d26d0d56b1f036526816d1787b169c1
devops-pritam <132892500+devops-pritam@users.noreply.github.com>
2023-10-28 02:07:40 +0100

    Update 04.sum_and_average_List.py

--- 04.sum_and_average_List.py
+++ 04.sum_and_average_List.py
@@ -1,17 +1,1 @@
-# Find sum and average of List in Python
-# input: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# Output:
-# sum =  55
-# average =  5.5
 
-import statistics
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 
- 
-# using sum() method
-sum1 = sum(my_list)
- 
-# finding average
-avg = statistics.mean(my_list)
- 
-print("sum = ", sum1)
-print("average = ", avg)
